0b507b055d77a0be274dc34de876985135116d72
Abu Hasan <hasanabubachelor@gmail.com>
2021-09-29 10:55:02 +0600

    Project Finished

---
+++ count.js
@@ -0,0 +1,31 @@
+const increaseBtn = document.getElementById('increaseBtn');
+const decreaseBtn = document.getElementById('decreaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const countValue = document.getElementById('count-value');
+let countNumber = 0;
+// increaseBtn.addEventListener('click',function(){
+//     countNumber++;  
+//     countValue.innerText = countNumber;
+// });
+// decreaseBtn.addEventListener('click',function(){
+//     countNumber--;  
+//     countValue.innerText = countNumber; 
+// });
+// resetBtn.addEventListener('click',function(){
+//     countNumber = 0; 
+//     countValue.innerText = countNumber; 
+// });
+
+//Optimize Code into one function
+function counter(isTrue){
+    if(isTrue == true){
+        countNumber++;  
+    }
+    else if(isTrue==false && countNumber>0){
+        countNumber--;  
+    }
+    else{
+        countNumber = 0; 
+    }
+    countValue.innerText = countNumber; 
+}
